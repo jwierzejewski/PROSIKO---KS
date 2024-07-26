800d8d4ff400314db1dfd2455dd1de79a4641c54
jwierzejewski <jacek.wierzejewski@gmail.com>
2024-07-26 14:28:04 +0200

    init commons

---
+++ Commons/CommandTools.cs
@@ -0,0 +1,90 @@
+﻿using System.Text;
+
+namespace Commons
+{
+    public static class CommonTools
+    {
+        static public string Trush(int n)
+        {
+            char[] answer = new char[n];
+            int m = 'Z' - 'A' + 1;
+            for (int i = 0; i < n; i++)
+            {
+                answer[i] = (char)('A' + i % m);
+            }
+            return new string(answer);
+        }
+
+        public static string SubstringMax(this string str, int len)
+        {
+            return str.Substring(0, Math.Min(len,str.Length)).Replace("\n","");
+        }
+
+        public static string GetParam(string command, int paramNumber, bool removeEndLines = true)
+        {
+            try
+            {
+                if (removeEndLines)
+                    command = command.Replace("\n", "");
+
+                int firstIdx = -1;
+                int secondIdx = 0;
+                for (int i = 0; i < paramNumber; i++)
+                {
+                    firstIdx = command.IndexOf(" ", firstIdx + 1);
+                }
+                for (int i = 0; i <= paramNumber; i++)
+                {
+                    secondIdx = command.IndexOf(" ", secondIdx + 1);
+                    if (secondIdx == -1)
+                        secondIdx = command.Length;
+                }
+                return command.Substring(firstIdx + 1, secondIdx - firstIdx - 1);
+            }
+            catch
+            {
+                return "";
+            }
+        }
+
+        public static string FromSpecifiedDelimeterToEnd(string text, int startDelimeterNumber, char delimeter=' ',bool removeEndLines = true)
+        {
+            if (removeEndLines)
+                text = text.Replace("\n", "");
+
+            int startIdx = -1;
+            for (int i = 0; i < startDelimeterNumber; i++)
+            {
+                startIdx = text.IndexOf(delimeter, startIdx+1);
+            }
+            if (startIdx == -1)
+            {
+                return "";
+            }
+            string result = text.Substring(startIdx + 1);
+
+            return result;
+        }
+
+        public static string CreateFilePath(string directoryPath, string fileName)
+        {
+            return directoryPath + "\\" + fileName;
+        }
+
+        public static string EncodeToBase64(string input)
+        {
+            byte[] bytes = Encoding.UTF8.GetBytes(input);
+            string output = Convert.ToBase64String(bytes);
+            return output;
+        }
+
+        public static string DecodeFromBase64(string input)
+        {
+            byte[] bytes = Convert.FromBase64String(input);
+            string output = Encoding.UTF8.GetString(bytes);
+            return output;
+        }
+
+
+    }
+}
---
+++ Commons/UDPPackage.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Commons
+{
+    public static class UDPPackage
+    {
+        public static void SendPackages(this UdpClient udpClient, string data,IPEndPoint endPoint = null)
+        {
+            
+            byte[] countBytes, numberBytes;
+            int MaxSize = 65000;
+            int ConfSize = 8;
+            byte[] dataBytes = Encoding.UTF8.GetBytes(data);
+            var packageList = createPackages(dataBytes,MaxSize-ConfSize);
+            for (int j = 0; j < packageList.Count; j++)
+            {
+                byte[] packageData = packageList[j];
+                byte[] sendingPackage = new byte[packageData.Length + 8];
+
+                countBytes = BitConverter.GetBytes(packageList.Count());
+                countBytes.CopyTo(sendingPackage, 0);
+
+                numberBytes = BitConverter.GetBytes(j);
+                numberBytes.CopyTo(sendingPackage, 4);
+
+                packageData.CopyTo(sendingPackage, 8);
+
+                udpClient.Send(sendingPackage, sendingPackage.Length,endPoint);
+                if (packageList.Count - 1 != j)
+                    Thread.Sleep(1);
+            }
+        }
+
+        public static string RecievePackages(this UdpClient udpClient, ref IPEndPoint remoteIpEndPoint)
+        {
+            string res;
+            int count, number;
+            int i = 0;
+            byte[] data;
+            PriorityQueue<string, int> priorityQueue = new();
+
+            data = udpClient.Receive(ref remoteIpEndPoint);
+            count = processPackage(data, priorityQueue);
+            i++;
+
+            
+            while(count>i)
+            {
+                data = udpClient.Receive(ref remoteIpEndPoint);
+                processPackage(data, priorityQueue);
+                i++;
+            }
+
+            res = string.Empty;
+            while (priorityQueue.Count > 0)
+                res += priorityQueue.Dequeue();
+
+            return res;
+        }
+
+        private static int processPackage(byte[] data, PriorityQueue<string, int> priorityQueue)
+        {
+            byte[] countBytes = new byte[4];
+            byte[] numberBytes = new byte[4];
+
+            Array.Copy(data, 0, countBytes, 0, 4);
+            int count = BitConverter.ToInt32(countBytes, 0);
+            Array.Copy(data, 4, numberBytes, 0, 4);
+            int number = BitConverter.ToInt32(numberBytes, 0);
+            string message = Encoding.UTF8.GetString(data, 8, data.Length - 8);
+
+            priorityQueue.Enqueue(message, number);
+
+            return count;
+        }
+
+
+        private static List<byte[]> createPackages(byte[] fullData, int packageSize)
+        {
+            int dataStart = 0;
+            List<byte[]> packageList = new();
+            while (dataStart < fullData.Length)
+            {
+                byte[] package = fullData.Skip(dataStart).Take(Math.Min(packageSize, fullData.Length-dataStart)).ToArray();
+                packageList.Add(package);
+                dataStart += packageSize;
+            }
+            return packageList;
+        }
+    }
+}
